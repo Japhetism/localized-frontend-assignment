4d7425fbf86377017e4d608b2ea3e5241b5e6d48
Japhetism <babatundeojo30@gmail.com>
2021-06-20 20:20:44 +0100

    add login screen

---
+++ src/screens/authentication/login/components/login-form.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { 
+    FormHelperText, TextField, Link, InputAdornment, 
+    IconButton, Button, Typography, makeStyles, Container 
+} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+    email: yup
+      .string('Enter your email')
+      .email('Please enter a valid email address')
+      .required('Email is required'),
+    password: yup
+      .string('Enter your password')
+      .min(8, 'Password should be of minimum 8 characters length')
+      .required('Password is required')
+  });
+
+const LoginForm  = (props) => {
+
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = React.useState(false);
+    const { handleSubmit } = props;
+
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            handleSubmit(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <Container maxWidth="xl" className={classes.root}>
+            <form onSubmit={formik.handleSubmit} className={classes.form}>
+                <div className={classes.input}>
+                    <TextField
+                        fullWidth
+                        id="outlined-error-helper-email"
+                        name="email"
+                        variant="outlined"
+                        label="Email Address"
+                        defaultValue={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                    />
+                    <FormHelperText className="MuiFormHelperText-root Mui-error">{formik.touched.email && formik.errors.email}</FormHelperText>
+                </div>
+                <div>
+                    <TextField
+                        fullWidth
+                        id="outlined-error-helper-password"
+                        variant="outlined"
+                        name="password"
+                        label="Password"
+                        type={showPassword ? 'text' : 'password'}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    onMouseDown={(event) => event.preventDefault()}
+                                    edge="end"
+                                    >
+                                    {!showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                        defaultValue={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                    />
+                    <FormHelperText className="MuiFormHelperText-root Mui-error">{formik.touched.password && formik.errors.password}</FormHelperText>
+                </div>
+                <Button fullWidth variant="contained" size="large" type="submit" className={classes.button}>
+                    Log In
+                </Button>
+            </form>
+        </Container>
+    );
+}
+
+const useStyles = makeStyles(() => ({
+    root: {
+        backgroundColor: '#FFFFFF',
+        width: '150%',
+        borderRadius: '3%',
+        boxShadow: '5px 5px 5px #aaaaaa'
+    },
+    form: {
+        width: '100%',
+        paddingTop: '5%',
+        paddingBottom: '10%'
+    },
+    input: {
+        marginBottom: '5%'
+    },
+    button: {
+        backgroundColor: '#1877F2',
+        color: '#FFFFFF',
+        marginBottom: '10%',
+        marginTop: '5%',
+        lineHeight: '30px',
+        fontWeight: 'bold',
+        fontSize: '20px',
+        '&:hover': {
+            backgroundColor: '#1877F2',
+            color: '#FFFFFF',
+        }
+    }
+}));
+
+export default LoginForm;
--- src/screens/authentication/login/loginView.js
+++ src/screens/authentication/login/loginView.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Box, Container, makeStyles } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import LoginForm from './components/login-form';
 
 const LoginView = () => {
-    return <div>
-        <span>Login Screen here</span>
-    </div>
+    
+    const classes = useStyles();
+
+    return <Container maxWidth="xl" className={classes.root}>
+        <Box display="flex">
+            <Box p={10}>
+                <Typography variant="h4" component="h4" className={classes.headerTitle}>
+                    Facebook
+                </Typography>
+                <Typography variant="body1" component="span" className={classes.headerDescription}>
+                    Facebook helps you connect and share with the people in your life.
+                </Typography>
+            </Box>
+            <Box p={10}>
+                <LoginForm />
+            </Box>
+        </Box>
+  </Container>
 }
+
+const useStyles = makeStyles(() => ({
+    root: {
+        backgroundColor: '#F0F2F5',
+        height: '100vh',
+        paddingTop: "10vh"
+    },
+    headerTitle: {
+        color: '#1877F2',
+        fontWeight: "bold",
+        fontSize: "40px"
+    },
+    headerDescription: {
+        fontFamily: "SFProDisplay-Regular, Helvetica, Arial, sans-serif",
+        fontSize: "20px",
+        fontWeight: "normal",
+        lineHeight: "32px",
+        width: "500px"
+    }
+}))
 
 export default LoginView;
